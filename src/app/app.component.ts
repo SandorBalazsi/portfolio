9b20d108991cb8ff812d909b4c4cefd00ff8eda9
SandorBalazsi <166751447+SandorBalazsi@users.noreply.github.com>
2025-01-11 18:06:06 +0100

    started on landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { LandingPageComponent } from './main-content/landing-page/landing-page.component';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AboutMeComponent } from './main-content/about-me/about-me.component';
+import { SkillsComponent } from './main-content/skills/skills.component';
+import { ProjectsComponent } from './main-content/projects/projects.component';
+import { ReferenceComponent } from './main-content/reference/reference.component';
+import { ContactComponent } from './main-content/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandingPageComponent, HeaderComponent, FooterComponent, AboutMeComponent, SkillsComponent,ProjectsComponent,ReferenceComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
