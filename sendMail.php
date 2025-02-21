cea98c4042bb356abd1e4bc95ded03cc5340d6ba
SandorBalazsi <166751447+SandorBalazsi@users.noreply.github.com>
2025-02-21 12:35:38 +0100

    worked on php

--- sendMail.php
+++ sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'sandor.b@gmx.ch';  
+            $recipient = 'sinya93@gmail.com';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
