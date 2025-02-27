ee8b59b11815ff48afee9357cd2b8650382c0fb3
SandorBalazsi <166751447+SandorBalazsi@users.noreply.github.com>
2025-02-27 11:08:41 +0100

    worked on burger icon and some other small changes

--- sendMail.php
+++ sendMail.php
@@ -6,32 +6,35 @@ switch ($_SERVER['REQUEST_METHOD']) {
         header("Access-Control-Allow-Methods: POST");
         header("Access-Control-Allow-Headers: content-type");
         exit;
-        case("POST"): //Send the email;
-            header("Access-Control-Allow-Origin: *");
-            // Payload is not send to $_POST Variable,
-            // is send to php:input as a text
-            $json = file_get_contents('php://input');
-            //parse the Payload from text format to Object
-            $params = json_decode($json);
-    
-            $email = $params->email;
-            $name = $params->name;
-            $message = $params->message;
-    
-            $recipient = 'sandor.b@gmx.ch';  
-            $subject = "Contact From <$email>";
-            $message = "From:" . $name . "<br>" . $message ;
-    
-            $headers   = array();
-            $headers[] = 'MIME-Version: 1.0';
-            $headers[] = 'Content-type: text/html; charset=utf-8';
-
-            // Additional headers
-            $headers[] = "From: noreply@mywebsite.com";
-
-            mail($recipient, $subject, $message, implode("\r\n", $headers));
+    case("POST"): //Send the email;
+        if ($_ENV["API_KEY"] !== _SERVER["API_KEY"]) {
             break;
-        default: //Reject any non POST or OPTIONS requests.
-            header("Allow: POST", true, 405);
-            exit;
+        }
+        header("Access-Control-Allow-Origin: *");
+        // Payload is not send to $_POST Variable,
+        // is send to php:input as a text
+        $json = file_get_contents('php://input');
+        //parse the Payload from text format to Object
+        $params = json_decode($json);
+
+        $email = $params->email;
+        $name = $params->name;
+        $message = $params->message;
+
+        $recipient = 'sandor.b@gmx.ch';  
+        $subject = "Contact From <$email>";
+        $message = "From:" . $name . "<br>" . $message ;
+
+        $headers   = array();
+        $headers[] = 'MIME-Version: 1.0';
+        $headers[] = 'Content-type: text/html; charset=utf-8';
+
+        // Additional headers
+        $headers[] = "From: noreply@mywebsite.com";
+
+        mail($recipient, $subject, $message, implode("\r\n", $headers));
+        break;
+    default: //Reject any non POST or OPTIONS requests.
+        header("Allow: POST", true, 405);
+        exit;
     } 
